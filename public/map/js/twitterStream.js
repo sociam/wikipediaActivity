2d0493686d299729d8e4f67d5ea603e7df3deb0f
Ramine Tinati <raminetinati@gmail.com>
2014-11-20 12:32:05 +0000

    updated with new location methods

--- public/map/js/twitterStream.js
+++ public/map/js/twitterStream.js
@@ -19,14 +19,17 @@ function initialize() {
   
   var light_grey_style = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]}];
   
+  var midnight = [ { "featureType": "water", "stylers": [ { "color": "#021019" } ] }, { "featureType": "landscape", "stylers": [ { "color": "#08304b" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#0c4152" }, { "lightness": 5 } ] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b434f" }, { "lightness": 25 } ] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b3d51" }, { "lightness": 16 } ] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [ { "color": "#000000" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#ffffff" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#000000" }, { "lightness": 13 } ] }, { "featureType": "transit", "stylers": [ { "color": "#146474" } ] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 } ] } ];
+
   var myOptions = {
     zoom: 3,
+    scrollwheel: false,
     center: myLatlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     streetViewControl: false,
     panControl: false,
-    zoomControl: true,
+    zoomControl: false,
     zoomControlOptions: {
       style: google.maps.ZoomControlStyle.SMALL
     },
@@ -40,8 +43,9 @@ function initialize() {
   var liveTweetsPos = new google.maps.MVCArray();
     heatmapPos = new google.maps.visualization.HeatmapLayer({
     data: liveTweetsPos,
-    radius: 25
+    radius: 15
   });
+    
   var gradient = [
     'rgba(30,244,248,0)',
     'rgba(30,244,248,0.5)',
@@ -59,17 +63,35 @@ function initialize() {
   var liveTweetsNeg = new google.maps.MVCArray();
     heatmapNeg = new google.maps.visualization.HeatmapLayer({
     data: liveTweetsNeg,
-    radius: 15
+    radius: 5
   });
-  var gradient = [
-    'rgba(232,213,46,0)',
-    'rgba(232,213,46,0.25)',
-    'rgba(232,213,46,0.5)',
-    'rgba(234,134,46,0.5)',
-    'rgba(236,128,46,0.5)',
-    'rgba(255,50,50,0.5)',
-    'rgba(255,50,50,0.5)'
+ // var gradient = [
+ //    'rgba(30,244,248,0)',
+ //    'rgba(30,244,248,0.5)',
+ //    'rgba(30,244,248,0.5)',
+ //    'rgba(32,248,158,0.5)',
+ //    'rgba(33,249,152,0.5)',
+ //    'rgba(51,255,75,0.5)',
+ //    'rgba(51,255,75,0.5)'
+ //  ]
+
+ var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
   ]
+
   heatmapNeg.set('gradient', heatmapNeg.get('gradient') ? null : gradient);
   heatmapNeg.setMap(map);
 
@@ -82,23 +104,29 @@ function initialize() {
     socket.on('wikipedia_revisions', function (data) {
 	
 	//get geo wikipedia entries
-        if(data.wikipedia_language_geo != undefined){
-        // console.log(data.wikipedia_page_name);
-          var tweetLocation = new google.maps.LatLng(data.wikipedia_language_geo.lat,data.wikipedia_language_geo.lng);
+        if(data.wikipedia_user.user_geo != undefined){
+         console.log(data.wikipedia_page_name);
+          var tweetLocation = new google.maps.LatLng(data.wikipedia_user.user_geo.lat,data.wikipedia_user.user_geo.lng);
           liveTweetsPos.push({location: tweetLocation, weight: 10});
+
+          //now plot what wiki language set they are editing...
+          if(data.wikipedia_language_geo != undefined){
+
+            var wikiVersion = new google.maps.LatLng(data.wikipedia_language_geo.lat,data.wikipedia_language_geo.lng);
+            liveTweetsNeg.push({location: wikiVersion, weight: (-5)});
+          }
+
         }	
     });
 
     socket.on('tweets', function (data) {
-     
-
         // update words
         if(data.geo.lat != undefined){
          //console.log(data);
 	if(data.text.indexOf("wiki") > -1){
 	  //console.log(data.text)
           var tweetLocation = new google.maps.LatLng(data.geo.lat, data.geo.lng);
-          liveTweetsNeg.push({location: tweetLocation, weight: (-10)});
+          liveTweetsNeg.push({location: tweetLocation, weight: (5)});
 	  }
         }	
     });	
@@ -108,6 +136,7 @@ function initialize() {
  //    socket.on('spinn3r', function (data) {
  //        // update words
 	// if(data.text != undefined){
+ //    console.log(data);
 	// 	if(data.text.indexOf("wikipedia.org") > -1){
  //         	console.log(data);
  //        	}
@@ -117,17 +146,21 @@ function initialize() {
 
 function pop_arrays(){
 	try{
-		for (i = 0; i < (liveTweetsPos.getLength()-2); i++) { 
+		for (i = 0; i < (liveTweetsPos.getLength()-1); i++) { 
 			liveTweetsPos.pop();
-			console.log("popping");
+			//console.log("popping");
 		}
+    for (i = 0; i < (liveTweetsNeg.getLength()-1); i++) { 
+      liveTweetsNeg.pop();
+      //console.log("popping");
+    }
 	}catch(err){}
 	
 
 }		
 
 
-    var interval = setInterval(function(){pop_arrays()}, 3000);
+    var interval = setInterval(function(){pop_arrays()}, 2000);
 
 
 
